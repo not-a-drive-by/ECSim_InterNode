dc1097a755a174de21f70f7d9b055d06ed793285
not-a-drive-by <1226872555@qq.com>
2023-03-24 20:25:37 +0800

    generate test result

---
+++ ECSim/EdgeCloudSim/src/edu/boun/edgecloudsim/network/Channel.java
@@ -0,0 +1,59 @@
+package edu.boun.edgecloudsim.network;
+
+import edu.boun.edgecloudsim.edge_client.MobileDevice;
+import edu.boun.edgecloudsim.edge_server.EdgeDataCenter;
+import edu.boun.edgecloudsim.utils.Variable;
+
+import java.util.Random;
+
+public class Channel {
+    private MobileDevice mobileDevice;
+    private EdgeDataCenter edgeServer;
+
+    public double ratio;
+    public double distance;
+    public boolean usedFlag;
+    private Random r = new Random();
+
+    public Channel( MobileDevice mobileDevice, EdgeDataCenter edgeServer){
+        this.mobileDevice = mobileDevice;
+        this.edgeServer = edgeServer;
+
+        updateRatio();
+        this.usedFlag = false;
+    }
+
+    private double getDistance(){
+        double dx = mobileDevice.getX()-edgeServer.getX();
+        double dy = mobileDevice.getY()-edgeServer.getY();
+        return Math.sqrt( Math.pow(dx,2) + Math.pow(dy,2) );
+    }
+
+    //更新信道传输速率
+    public void updateRatio(){
+        distance = getDistance();
+        double channelGain = Variable.expRnd(1)*Math.pow(distance,-3);
+        double SINR = channelGain*mobileDevice.getPower()/Math.pow(10,-9);
+//        int ran = r.nextInt()<0.5 ? -3 : 3;
+        this.ratio = Math.log(1+SINR)/Math.log(2);
+        this.ratio /= 8;
+//        if(edgeServer.getId() == 8002){
+//            this.ratio /= 2;//设置一个质量较差的信道
+//        }
+    }
+
+    //一些没啥用的函数
+    public MobileDevice getMobileDevice() {    return mobileDevice;    }
+    public EdgeDataCenter getEdgeServer() {      return edgeServer;    }
+    public void setRatio(double newRatio) {  this.ratio = newRatio;  }
+
+    @Override
+    public String toString() {
+        return "Channel{" +
+                "mobileDevice=" + mobileDevice.getMobileID() +
+                ", edgeServer=" + edgeServer.getId() +
+                ", ratio=" + ratio +
+                ", usedFlag=" + usedFlag +
+                '}' + "\r\n";
+    }
+}
---
+++ ECSim/EdgeCloudSim/src/edu/boun/edgecloudsim/utils/StaticfinalTags.java
@@ -0,0 +1,22 @@
+package edu.boun.edgecloudsim.utils;
+
+public final class StaticfinalTags {
+    /**penalty项系数**/
+    public static final double alpha = 4;
+
+    /**任务数量**/
+    public static final int CloudletNum = 200;
+
+    /**当前时间, 如果为负数，则无效*/
+    public static int curTime=0;
+
+    /**设备参数*/
+    public static double lambda1 = 0.09; // 0.05job/slot
+    public static double lambda2 = 0.09;
+    public static double lambda3 = 0.09;
+
+    public static double channelThres = 0.5;
+    public static double disThres = 100;
+
+    //分布的参数
+}
